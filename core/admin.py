288b4ce60dc84d2fcdf1587356401a91b3ccda19
elhassane <elhassanegargem@gmail.com>
2020-04-13 00:31:45 +0200

    add admin

--- core/admin.py
+++ core/admin.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
-from core.models import Deal
+from core.models import Deal, Location, Category
 
 
 class DealAdmin(admin.ModelAdmin):
     pass
 
+
+class LocationAdmin(admin.ModelAdmin):
+    pass
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(Deal,DealAdmin)
+admin.site.register(Location,LocationAdmin)
+admin.site.register(Category,CategoryAdmin)
 
 # Register your models here.
