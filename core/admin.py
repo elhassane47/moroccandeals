24ea26424e0fd8e1f4f3751bf47eab8cfff843c9
elhassane <elhassanegargem@gmail.com>
2020-04-13 00:38:23 +0200

    add fonts

--- core/admin.py
+++ core/admin.py
@@ -1,21 +1,10 @@
 from django.contrib import admin
-from core.models import Deal, Location, Category
+from core.models import Deal
 
 
 class DealAdmin(admin.ModelAdmin):
     pass
 
-
-class LocationAdmin(admin.ModelAdmin):
-    pass
-
-
-class CategoryAdmin(admin.ModelAdmin):
-    pass
-
-
 admin.site.register(Deal,DealAdmin)
-admin.site.register(Location,LocationAdmin)
-admin.site.register(Category,CategoryAdmin)
 
 # Register your models here.
