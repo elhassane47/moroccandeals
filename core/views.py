c17d58e794191548989e797515ce729e46c75927
elhassane <elhassanegargem@gmail.com>
2020-04-15 01:46:58 +0200

    list of categories and number

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.db.models import Count
 from django.views.generic import View, TemplateView, ListView, DetailView
-from core.models import Deal
+from core.models import Deal, Category
 
 
 def deal_details(request, slug, deal_id, form=None):
@@ -10,4 +11,10 @@ def deal_details(request, slug, deal_id, form=None):
 class DealsList(ListView):
     model = Deal
     paginate_by = 20
+
+    def get_context_data(self, **kwargs):
+        context = super(DealsList, self).get_context_data(**kwargs)
+        context['categories'] = Deal.objects.all().values('category__name').annotate(total=Count('category')).order_by('total')
+        return context
+
 
