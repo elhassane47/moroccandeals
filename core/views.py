a4db2fb831786004710b3a881a2ef0eb0fb9ed97
elhassane <elhassanegargem@gmail.com>
2020-04-15 13:41:18 +0200

    detail deal

--- core/views.py
+++ core/views.py
@@ -1,11 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.db.models import Count
 from django.views.generic import View, TemplateView, ListView, DetailView
 from core.models import Deal, Category
 
 
 def deal_details(request, slug, deal_id, form=None):
-    pass
+    deal = get_object_or_404(Deal, pk=deal_id)
+
+    context = {}
+
+    context['object'] = deal
+    context['more_deals'] = Deal.objects.filter(user=deal.user)[:3]
+    return render(request, 'core/deal_detail.html', context)
+
 
 
 class DealsList(ListView):
@@ -17,4 +24,18 @@ class DealsList(ListView):
         context['categories'] = Deal.objects.all().values('category__name').annotate(total=Count('category')).order_by('total')
         return context
 
+
+# class DealDetail(DetailView):
+#     model = Deal
+def ledger1_detail_view(request, pk1, pk2):
+
+    get_object_or_404(company, pk=pk1)
+    ledger = get_object_or_404(ledger1, pk=pk2)
+
+    context = {}
+    context['ledger1_details'] = ledger
+    # Add other items to the context
+    ...
+
+    return render(request, 'accounting_double_entry/ledger1_details.html', context)
 
